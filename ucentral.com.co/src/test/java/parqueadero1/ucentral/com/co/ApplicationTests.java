c7ccc20c2ad45b7baff8c3f0734b3a6dc17e6bb8
yvillanueva <yvillanuevap1@ucentral.edu.co>
2024-09-28 21:10:27 -0500

    primer repositorio de parqueadero

---
+++ ucentral.com.co/src/test/java/parqueadero1/ucentral/com/co/ApplicationTests.java
@@ -0,0 +1,13 @@
+package parqueadero1.ucentral.com.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
